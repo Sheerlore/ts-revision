85e7c23fb08e714374bb6ff2c66a959bb4f1c15b
uai <noema01@outlook.jp>
2021-10-02 13:16:36 +0900

    github info fetch

--- pra2/src/App.tsx
+++ pra2/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+
+const loadJSON = (key: string) =>
+  key && JSON.parse(localStorage.getItem(key));
+
+const saveJSON = (key, data) =>
+  localStorage.setItem(key, JSON.stringify(data));
+
+function GitHubUser({ login }: {login:string}) {
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then(response => response.json())
+      .then(setData)
+      .catch(console.error);
+  }, [login]);
+
+  if (data) {
+    return <pre>{JSON.stringify(data, null, 2)}</pre>
+  }
+  return null;
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GitHubUser login="Sheerlore" />
     </div>
   );
 }
