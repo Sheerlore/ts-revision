c60136f402fa516b8b480fcf371348d669f7f458
uai <noema01@outlook.jp>
2021-10-04 19:14:47 +0900

    Fetch component make

---
+++ pra3/src/Fetch.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useFetch } from "./hook";
+
+interface FetchProps {
+  uri: string,
+  renderSuccess: (params: any) => React.Component,
+  loadingFallback: JSX.Element
+  renderError: (params: string) => JSX.Element
+}
+
+export default function Fetch({
+  uri,
+  renderSuccess,
+  loadingFallback = <p>loading...</p>,
+  renderError = (error: string) => (
+    <pre>{JSON.stringify(error, null, 2)}</pre>
+  )
+}: FetchProps) {
+  const { loading, data, error } = useFetch(uri);
+  if (error) return renderError(error);
+  if (loading) return loadingFallback;
+  if (data) return renderSuccess(data)
+}
